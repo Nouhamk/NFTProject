518738257678eb05ef899d74bba152e8ca844e53
emmanuel chappat <emmanuel@liveart.io>
2023-11-28 09:19:48 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# Solidity Hexagon test
+
+
+You will find in this file the correction for the "fake NFT" done in class.
+
+Make sure to read it and understand it well. 
+
+## Solidity Written Exam
+
+Add the follwing functionalities to this code:
+
+1. In this code, there is one issue (there are actually a few other beyond the scope for this class), a single address can only own a single NFT. If a user purchase a new NFT then the previous one will be errased. The goal is to solve this issue (there are many approaches)
+
+2. Create a function `Transfer(uint256 id, address recipient)` that allow the owner of an NFT to send it to someone else
+
+3. (optinal) Allow the owner of an NFT, to put an NFT on sell. ie. A user can set a new price to one their NFTs and mark it as "on-sale". Any user can then purchase that NFT for the correct amount. The funds from the sell should lend in the previous owner wallet (We have not done this in class but refer to this: https://solidity-by-example.org/sending-ether/)
+
+
+
+### Rating
+
+You will be rated as follow:
+
+#### Coding
+See exercie above
+
+#### Oral
+1. Answer questions regarding the general understanding of blockchain. example:
+
+- How blockchain work
+- Different type of blockchain
+- Consensus algorithm
+- How to interact with a blockchain
+- How to use metamask
+- Security rules for using a wallet
+and more 
+
+2. You will have to explain the code that you written above. So make sure you understand it properly
+
+
+Good luck !
+
+
+
+
+
+
---
+++ src/NFT.sol
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+// This is a non standard NFT like contract
+contract NFT {
+
+    // This struct define the shape of the data that we want
+    // to store for each NFT
+    struct NFTData {
+        string text;
+        address owner;
+        uint256 id;
+        uint256 price;
+    }
+
+    // This is a list of NFTData
+    NFTData[] public NFTs;
+
+    // Mapping to store the NFT for a given owner.
+    mapping(address => uint256) public NFTsByOwner;
+
+    // Here we store the admin address 
+    address admin;
+
+    constructor () {
+        // Here we store the address of the deployer account
+        // so that we can refer to it to check if someone is admin
+        admin = msg.sender;
+    }
+    
+    // This function will create new NFT for sale
+    function mint(string memory newText, uint256 newPrice) public {
+        require(msg.sender == admin, "Only admin can mint");
+
+        // Create an NFTData and push it in the list of NFTs
+        NFTs.push(NFTData({
+            text: newText,
+            // We set address 0 (NULL address) as owner for now
+            owner: address(0),
+            // We set the id of the NFT
+            id: NFTs.length,
+            price: newPrice
+        }));
+    }
+        
+
+    // Here the user can purchase an NFT
+    // This function is payable so that user can send funds to this function
+    function buyNFTById( uint256 id) public payable {
+        NFTData storage token = NFTs[id];
+
+        // If the price is not set, it likely mean that this NFT doesn't exist
+        require(token.price > 0, "This NFT does not exist");
+
+        // Check that the price match
+        require(token.price > msg.value, "Not enough funds sent");
+
+        // If all check passed, now we can update the NFT owner
+        token.owner = msg.sender;
+        NFTsByOwner[msg.sender] = token.id; 
+    }
+
+}
